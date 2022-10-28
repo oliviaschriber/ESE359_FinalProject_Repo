4e48f2a664e0054ba49d1a0c92fd6fe370a02cf7
cameronkalik <c.e.kalik@wustl.edu>
2022-10-28 12:56:52 -0500

    nice

--- bias.py
+++ bias.py
@@ -1,4 +1,23 @@
-print('jeng nov 8th olivia sept 7th')
+import requests
 
-print('cameron may 8th')
-print('happy birthday 2 uuuuuuuu')
+pronouns = ['she', 'her', 'woman']
+text = "she is a woman" #Scraped from web
+words = text.split(' ')
+print(words)
+
+url = "https://www.vanityfair.com/news/2022/10/marjorie-taylor-greene-abortion-call-in"
+page = requests.get(url)
+
+print(page.text)
+
+def bias_detection():
+    bias_count = 0
+    for i in pronouns:
+        if i in words:
+            bias_count = bias_count + 1
+    return bias_count
+
+
+print(bias_detection())
+
+
