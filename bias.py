d9f070fc357f865630a69df3eb362b250a00a57e
Cassie Jeng <59851290+cjeng8771@users.noreply.github.com>
2022-11-02 12:44:07 -0500

    working codevim bias.pyvim bias.py

--- bias.py
+++ bias.py
@@ -1,23 +1,53 @@
 import requests
+from bs4 import BeautifulSoup
+import colorama
+from colorama import Fore
 
-pronouns = ['she', 'her', 'woman']
-text = "she is a woman" #Scraped from web
-words = text.split(' ')
-print(words)
+# terms = ['she', 'her', 'woman']
+male_terms_job = ["active", "adventurous", "aggressive", "ambitious", "analytic", "assertive", "athletic", "autonomous", "battle", "boastful", "challenge", "champion", "competitive", "confident", "courageous", "decide", "decision", "decisive", "defend", "determined", "dominating", "dominant", "driven", "fearless", "fight", "forceful", "greedy", "head-strong", "headstrong", "hierarchical", "hostile", "impulsive", "independent", "individual", "intellectual", "lead", "logical", "objective", "opinionated", "outspoken", "persistent", "principle", "reckless", "self-confident", "self-reliant", "self-sufficient", "stubborn", "superior", "unreasonable"]
+female_terms_job = ["agreeable", "affectionate", "child", "cheering", "collaborative", "communal", "compassionate", "connecting", "considerate", "cooperative", "co-operative", "dependable", "emotional", "empathetic", "feeling", "flatterable", "gentle", "honest", "interdependent", "interpersonal", "kind", "kinship", "loyal", "modest", "nagging", "nurturing", "pleasant", "polite", "quiet", "responsive", "sensitive", "submissive", "supportive", "sympathetic", "tender", "trusting", "understanding", "warm", "whiney", "enthusiastic", "inclusive", "yielding", "share", "sharing"]
 
-url = "https://www.vanityfair.com/news/2022/10/marjorie-taylor-greene-abortion-call-in"
-page = requests.get(url)
+def listToString(list):
+	string = ""
+	for each in list:
+		string += each
+	return string
 
-# print(page.text)
+def bias_detection(terms):
 
-def bias_detection():
-    bias_count = 0
-    for i in pronouns:
-        if i in words:
-            bias_count = bias_count + 1
-    return bias_count
+	url = "https://www.vanityfair.com/news/2022/10/marjorie-taylor-greene-abortion-call-in" # Scraped from Web
+	page = requests.get(url)
 
+	soup = BeautifulSoup(page.text,'html.parser')
+	soup_text = soup.get_text()
+	# soup_text is <class 'str'>
+	# print(soup_text)
 
-print(bias_detection())
+	soup_arr = soup_text.split()
 
+	count = 0
+	term_indices = []
+	for word in soup_arr:
+		# print(word)
+		if word in terms:
+			term_indices.append(count) # index of word in list
 
+		count = count + 1
+	# print(term_indices)
+
+	count2 = 0
+	for word in soup_arr:
+		if count2 in term_indices:
+			print(Fore.RED + str(word) + Fore.GREEN, end=' ')
+		else: 
+			print(Fore.GREEN + str(word), end=' ')
+		count2 = count2 + 1
+
+	print('\ncounts by bias terms:')
+	for each in terms:
+		print(str(each) + ': ' + str(soup_arr.count(each)))
+
+print('Male_terms in job ads: ')
+bias_detection(male_terms_job)
+print('\nFemale_terms in job ads: ')
+bias_detection(female_terms_job)
