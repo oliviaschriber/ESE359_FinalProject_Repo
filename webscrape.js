35c9686e7cc86d99fd03c183167c357abd00865b
Cassie Jeng <59851290+cjeng8771@users.noreply.github.com>
2022-11-19 15:00:58 -0600

    working on url

--- webscrape.js
+++ webscrape.js
@@ -1,19 +1,17 @@
 male_terms_job = ["active", "adventurous", "aggressive", "ambitious", "analytic", "assertive", "athletic", "autonomous", "battle", "boastful", "challenge", "champion", "competitive", "confident", "courageous", "decide", "decision", "decisive", "defend", "determined", "dominating", "dominant", "driven", "fearless", "fight", "forceful", "greedy", "head-strong", "headstrong", "hierarchical", "hostile", "impulsive", "independent", "individual", "intellectual", "lead", "logical", "objective", "opinionated", "outspoken", "persistent", "principle", "reckless", "self-confident", "self-reliant", "self-sufficient", "stubborn", "superior", "unreasonable"];
 female_terms_job = ["agreeable", "affectionate", "child", "cheering", "collaborative", "communal", "compassionate", "connecting", "considerate", "cooperative", "co-operative", "dependable", "emotional", "empathetic", "feeling", "flatterable", "gentle", "honest", "interdependent", "interpersonal", "kind", "kinship", "loyal", "modest", "nagging", "nurturing", "pleasant", "polite", "quiet", "responsive", "sensitive", "submissive", "supportive", "sympathetic", "tender", "trusting", "understanding", "warm", "whiney", "enthusiastic", "inclusive", "yielding", "share", "sharing"];
 
-/*
-const getWebData = async() => {
-    const webdata = await fetch(url);
-    const webtext = await response.text();
-    document.getElementById("final_output").value = text.match(/(?<=\<h1>).*(?=\<\/h1>)/);
-}
-*/
-
 function submittext(){
     inputtext = '';
     outputtext = '';
 
     inputtext = document.getElementById("input_field").value;
+
+    if(inputtext.substring(0,4) == 'http'){
+        URLText = getURLText(inputtext);
+        console.log(URLText);
+    }
+
     outputtext = bias_detector(inputtext,male_terms_job);
     document.getElementById('final_output').innerHTML = outputtext;
     counts = listToString(count_terms(inputtext,male_terms_job));
@@ -78,4 +76,19 @@ function count_terms(in_text,terms){
     });
 
     return term_counts;
+}
+
+function getURLText(inputurl){
+    // read text from URL location
+    var request = new XMLHttpRequest();
+    request.open('GET', inputurl, true);
+    request.send(null);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader('Content-Type');
+            if (type.indexOf("text") !== 1) {
+                return request.responseText;
+            }
+        }
+    }
 }
